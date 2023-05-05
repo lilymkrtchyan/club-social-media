1c15572bbc3839ce72458c65a8a5fd5254edade2
Julia <jwz28@cornell.edu>
2023-05-04 23:19:12 -0400

    added create post feature

---
+++ components/CreatePost.tsx
@@ -0,0 +1,13 @@
+import AddIcon from '@mui/icons-material/Add';
+
+const CreatePost = () => {
+  return (
+    <div className="createpost">
+      <textarea className = "createposttext"></textarea>
+      <br></br>
+      <button className = "createpostbutton">Create</button>
+    </div>
+  )
+}
+
+export default CreatePost
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,9 @@ import React from "react"
 import Link from "next/link"
 import ExploreIcon from '@mui/icons-material/Explore';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import CreatePost from "./CreatePost"
+import AddIcon from '@mui/icons-material/Add';
+
 
 const Sidebar = () => {
   return (
@@ -9,14 +12,20 @@ const Sidebar = () => {
       <h1>Cornell Connect</h1>
       <ul >
         <li>
-          <ExploreIcon />
-          <Link href="/" className="Link">Explore</Link>
+            <ExploreIcon />
+
+            <Link href="/" className="Link">Explore</Link>
         </li>
         <li>
           <AccountCircleIcon />
           <Link href="/account" className="Link">Account</Link>
         </li>
+        <li>
+          <AddIcon /><span className="createpost">Create Post</span>
+          {/* <div><CreatePost /></div> */}
+        </li>
       </ul>
+      <CreatePost />
     </div>
   )
 }
