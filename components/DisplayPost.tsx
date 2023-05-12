8f767b731d2b10b35fedb76ed78731ecebb8b03e
Julia <jwz28@cornell.edu>
2023-05-12 10:54:14 -0400

    Added delete post button

--- components/DisplayPost.tsx
+++ components/DisplayPost.tsx
@@ -1,18 +1,27 @@
 import { PostWithId } from "@/types";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DeleteIcon from '@mui/icons-material/Delete';
 import Image from 'next/image'
+import {doc, deleteDoc, collection } from "firebase/firestore"
+import { db } from "@/util/firebase"
 
 type Props = {
   readonly post: PostWithId
 }
 
 const DisplayPost = ({ post }: Props) => {
+  const deletePost = () => {
+    const task = doc(db, "posts", post.id)
+    deleteDoc(task)
+  }
+
   return (
     <div className="post">
       <AccountCircleIcon className="profile" />
       <div className="postBody">
         <div className="postHeader">
           <h3>{post.name} {post.username}</h3>
+          <DeleteIcon className="deletepost" onClick={deletePost}/>
         </div>
         <p className="text">
           {post.text}
